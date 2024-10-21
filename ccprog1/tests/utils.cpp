7efd51e51ba973ab126b7cbd1bc6b0ef45020b32
CielSachen <manaallaire@outlook.com>
2024-10-22 00:24:41 +0800

    Clean-up custom test error message
    This removes the brackets aimed at matching the Google test result output. This also adds the missing Doxygen documentation for the function.

--- ccprog1/tests/utils.cpp
+++ ccprog1/tests/utils.cpp
@@ -3,12 +3,12 @@
 #include <cstdio>
 
 /**
- *
- * @param reopened_file
+ * Verifies if `freopen()` has successfully reopened a file.
+ * @param reopened_file The file that `freopen()` reopened.
  */
 void verify_file_reopening(FILE* reopened_file) {
   if (reopened_file == NULL) {
-    std::cerr << "[          ] The file that contains the mocked user inputs could not be opened." << std::endl;
+    std::cerr << "The file that contains the mocked user inputs could not be opened." << std::endl;
 
     exit(EXIT_FAILURE);
   }
